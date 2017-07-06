d11978b4b15ed6dc33ef56e3533555bc654590f8
reed-foster <reed.a.foster@gmail.com>
2017-07-06 16:09:01 -0700

    sketched out encode function, probably will switch to term-based parsing rather than sentence-based

--- NMEAParser.cpp
+++ NMEAParser.cpp
@@ -2,3 +2,50 @@
     NMEAParser.cpp - Library for parsing NMEA strings from a GPS
     Reed A. Foster, July 2017.
 */
+
+#include "NMEAParser.hpp"
+
+NMEAParser::NMEAParser()
+{
+
+}
+
+bool NMEAParser::encode(char c)
+{
+    bool valid_sentence = false;
+    switch (c)
+    {
+        case '$': //start character
+            memset(_sentence, 0, sizeof(_sentence));
+            _sentenceidx = 0;
+            _sentence[_sentenceidx++] = c;
+            return false;
+        case '\n': //end character
+            if (_sentence[_sentenceidx - 4] == '*') //checksum exists
+            {
+                int checksum = _hexToInt(_sentence[_sentenceidx - 3]) * 16 + _hexToInt(_sentence[_sentenceidx]);
+                int parity = 0;
+                for (int i = 1; i < _sentenceidx - 4; i++)
+                {
+                    parity ^= _sentence[i];
+                }
+                if (parity != checksum) //invalid checksum
+                    return false;
+            }
+            else
+                return false;
+    }
+    if (_sentenceidx < sizeof(sentence) - 1)
+        _sentence[_sentenceidx++] = c;
+
+}
+
+int NMEAParser::_hexToInt(char c)
+{
+    if (c >= 'A' && c <= 'F')
+        return c - 'A' + 10;
+    else if (c >= 'a' && c <= 'f')
+        return c - 'a' + 10;
+    else
+        return c - '0';
+}
--- NMEAParser.hpp
+++ NMEAParser.hpp
@@ -2,3 +2,30 @@
     NMEAParser.hpp - Library for parsing NMEA strings from a GPS
     Reed A. Foster, July 2017.
 */
+
+#define NMEATERM_MAXLENGTH 15
+
+class NMEAParser
+{
+public:
+    NMEAParser();
+
+    bool encode(char c);
+
+private:
+
+    int _hexToInt(char c);
+
+    char _term[NMEASENTENCE_MAXLENGTH];
+    int _termidx;
+
+    const char _GPGGA_TERM[] = "GPGGA";
+    const char _GPGLL_TERM[] = "GPGLL";
+    const char _GPGSA_TERM[] = "GPGSA";
+    const char _GPGSV_TERM[] = "GPGSV";
+    const char _GPRMC_TERM[] = "GPRMC";
+    const char _GPVTG_TERM[] = "GPVTG";
+    const char _GPZDA_TERM[] = "GPZDA";
+
+    unsigned long _time, _date
+}
