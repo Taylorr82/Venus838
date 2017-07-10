7ffbaf8b4c9a73269cdf14b7ddf99faee4e21974
reed-foster <reed.a.foster@gmail.com>
2017-07-10 14:34:34 -0700

    modified for field testing

--- examples/GPSComm/GPSComm.ino
+++ examples/GPSComm/GPSComm.ino
@@ -4,8 +4,10 @@
 #include <NMEAParser.hpp>
 #include <Venus838.hpp>
 
+#define GPS_DEBUG_BAUDRATE 9600
+
 NMEAParser parser;
-//Venus838 gps(Serial1, 115200, false);
+Venus838 gps(Serial1, 115200, false);
 
 String errorcodes[5] = {"NORMAL", "NACK", "TIMEOUT", "INVALIDARG", "UNKNOWN"};
 
@@ -17,7 +19,14 @@ void setup()
     //while (!gps.available());
     //Serial.println(errorcodes[(int)gps.setBaudRate(19200, 1)]);
     //Serial.println("exiting setup");
-    Serial1.begin(9600);
+    //Serial1.begin(9600);
+    gps.setBaudRate(115200, 0);
+    gps.cfgNMEA(0b0000000, 0);
+    gps.cfgNMEA(NMEA_GGA, true, 0);
+    gps.cfgNMEA(NMEA_GSA, true, 0);
+    gps.cfgNMEA(NMEA_GSV, true, 0);
+    gps.cfgNMEA(NMEA_RMC, true, 0);
+    gps.setUpdateRate(20, 0);
     Serial.begin(9600);
 }
 
@@ -25,9 +34,9 @@ void loop()
 {
     bool newdata = false;
     char c;
-    while (Serial1.available())
+    while (gps.available())
     {
-        c = Serial1.read();
+        c = gps.read();
         //Serial.write(c);
         newdata = parser.encode(c);
     }
