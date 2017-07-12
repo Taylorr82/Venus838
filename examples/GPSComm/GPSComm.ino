4ea3995591fa2b1c36326cca0c6434496960196e
reed-foster <reed.a.foster@gmail.com>
2017-07-12 11:28:51 -0700

    moved back to indoors methods (with spoofer)

--- examples/GPSComm/GPSComm.ino
+++ examples/GPSComm/GPSComm.ino
@@ -7,7 +7,7 @@
 #define GPS_DEBUG_BAUDRATE 9600
 
 NMEAParser parser;
-Venus838 gps(Serial1, 115200, false);
+//Venus838 gps(Serial1, 115200, false);
 
 String errorcodes[5] = {"NORMAL", "NACK", "TIMEOUT", "INVALIDARG", "UNKNOWN"};
 
@@ -20,28 +20,30 @@ void setup()
     //Serial.println(errorcodes[(int)gps.setBaudRate(19200, 1)]);
     //Serial.println("exiting setup");
     //Serial1.begin(9600);
-    gps.setBaudRate(115200, 0);
+    /*gps.setBaudRate(115200, 0);
     gps.cfgNMEA(0b0000000, 0);
     gps.cfgNMEA(NMEA_GGA, true, 0);
     gps.cfgNMEA(NMEA_GSA, true, 0);
     gps.cfgNMEA(NMEA_GSV, true, 0);
     gps.cfgNMEA(NMEA_RMC, true, 0);
-    gps.setUpdateRate(20, 0);
+    gps.setUpdateRate(20, 0);*/
     Serial.begin(9600);
+    Serial1.begin(9600);
 }
 
 void loop()
 {
     bool newdata = false;
     char c;
-    while (gps.available())
+    while (Serial1.available())
     {
-        c = gps.read();
+        c = Serial1.read();
         //Serial.write(c);
         newdata = parser.encode(c);
     }
     if (newdata)
     {
+        long utctime = parser.getTime();
         long latitude = parser.getLatitude();
         long longitude = parser.getLongitude();
         long altitude = parser.getAltitude();
@@ -51,6 +53,6 @@ void loop()
         char numsats = parser.getNSats();
         char numsatsvisible = parser.getNSatsVisible();
         long snravg = parser.getSNR();
-        Serial.printf("%d,%d,%d,%d,%d,%d,%d,%d,%d\n", latitude, longitude, altitude, pdop, hdop, vdop, numsats, numsatsvisible, snravg);
+        Serial.printf("%d,%d,%d,%d,%d,%d,%d,%d,%d,%d\n", utctime, latitude, longitude, altitude, pdop, hdop, vdop, numsats, numsatsvisible, snravg);
     }
 }
